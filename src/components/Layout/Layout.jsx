d573419d3f50c0c2646867498080188d55e5644e
Лілія <lily.fed85@gmail.com>
2023-01-15 15:02:16 +0200

    add redux auth

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 
 const Layout = () => {
   return (
     <>
       <div>Тут буде heder з навігацією</div>
-      <Outlet />
+      <Suspense fallback={<div>loading...</div>}>
+        <Outlet />
+      </Suspense>
     </>
   );
 };
---
+++ src/utils/index.js
@@ -0,0 +1,2 @@
+export { theme } from './theme';
+export { GlobalStyle } from './GlobalStyle';
