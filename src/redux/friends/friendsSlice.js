8a9a6074b349650ed42caa2a9ffde02b101b8ae2
Лілія <lily.fed85@gmail.com>
2023-01-15 17:21:16 +0200

    add friends and news redux

---
+++ src/redux/friends/friendsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { getFriends } from './friendsOperations';
+
+const friendsSlice = createSlice({
+  name: 'friends',
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers: {
+    [getFriends.pending]: state => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [getFriends.fulfilled]: (state, { payload }) => {
+      state.items = payload;
+      state.isLoading = false;
+    },
+    [getFriends.rejected]: (state, { payload }) => {
+      state.error = payload;
+      state.isLoading = false;
+    },
+  },
+});
+
+export const friendsReducer = friendsSlice.reducer;
---
+++ src/redux/news/newsSelectors.js
@@ -0,0 +1,3 @@
+export const selectNews = state => state.news.items;
+export const selectIsLoading = state => state.news.isLoading;
+export const selectError = state => state.news.error;
