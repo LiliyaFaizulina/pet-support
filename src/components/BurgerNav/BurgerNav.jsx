504f800ad7fff6ba58e9c11b6d77cb338b1082dc
Лілія <lily.fed85@gmail.com>
2023-01-18 22:48:32 +0200

    fix bugs

--- src/components/BurgerNav/BurgerNav.jsx
+++ src/components/BurgerNav/BurgerNav.jsx
@@ -1,6 +1,10 @@
 import { useMedia } from 'react-use';
 import { useSelector } from 'react-redux';
 import { selectIsAuth } from 'redux/auth/authSelectors';
+import { UserNav } from 'components/UserNav/UserNav';
+import { AuthNav } from 'components/AuthNav/AuthNav';
+import { Logo } from 'components/Logo/Logo';
+import { Nav } from 'components/Nav/Nav';
 
 export const BurgerNav = () => {
   const isAuth = useSelector(selectIsAuth);
