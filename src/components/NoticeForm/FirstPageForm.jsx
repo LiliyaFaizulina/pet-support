cdde61f6fdba25c27309f513bcdd3604bd31d550
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2023-01-20 01:56:14 +0200

    Зробив функціонал, але треба розібратися чому не постить на сервер, може дані не такі відправляю, кол завтра потрібен

--- src/components/NoticeForm/FirstPageForm.jsx
+++ src/components/NoticeForm/FirstPageForm.jsx
@@ -1,4 +1,32 @@
 import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import './style.css';
+
+// NoticeForm (FormsValidation):
+// -* title - будь які літери.мін 2 макс 48, - (required);
+// -* name - будь які літери.мін 2 макс 16;
+// -* birthdate - дата в форматі 22.10.2022;
+// -* breed - будь які літери.мін 2 макс 24;
+// -* location - строка в форматі Місто, Область.Наприклад: Brovary, Kyiv або Akhtyrka, Sumy;
+// -* comments - будь які літери та символи.мін 8, макс 120 - (required);
+// -* price - число, не повинно починатися 0 - (required)
+
+// const AddPetSchema = Yup.object().shape({
+//   title: Yup.string()
+//     .min(2, 'Too Short!')
+//     .max(48, 'Too Long!')
+//     .required('Required'),
+//   name: Yup.string().min(2, 'Too Short!').max(16, 'Too Long!'),
+//   birthdate: Yup.date().min(new Date('01.01.2000')).max(new Date()),
+//   breed: Yup.string().min(2, 'Too Short!').max(24, 'Too Long!'),
+//   // location: Yup.string()
+//   comments: Yup.string()
+//     .min(2, 'Too Short!')
+//     .max(120, 'Too Long!')
+//     .required('Required'),
+//   price: Yup.number().required('Required'),
+// });
 
 function FirstPageForm({ formData, setFormData }) {
   return (
@@ -8,13 +36,34 @@ function FirstPageForm({ formData, setFormData }) {
         consectetur
       </h3>
       <div>
-        <button type="button" value={'lost-found'}>
+        <button
+          type="button"
+          value="lost-found"
+          className={formData.category === 'lost-found' ? 'active' : ''}
+          onClick={event =>
+            setFormData({ ...formData, category: event.target.value })
+          }
+        >
           lost/found
         </button>
-        <button type="button" value={'for-free'}>
+        <button
+          type="button"
+          value="for-free"
+          className={formData.category === 'for-free' ? 'active' : ''}
+          onClick={event =>
+            setFormData({ ...formData, category: event.target.value })
+          }
+        >
           in good hands
         </button>
-        <button type="button" value={'sell'}>
+        <button
+          type="button"
+          value="sell"
+          className={formData.category === 'sell' ? 'active' : ''}
+          onClick={event => {
+            setFormData({ ...formData, category: event.target.value });
+          }}
+        >
           sell
         </button>
       </div>
@@ -49,11 +98,11 @@ function FirstPageForm({ formData, setFormData }) {
         <div>
           <label>Date of birth</label>
           <input
-            type="text"
+            type="date"
             name="date"
-            defaultValue={formData.dateOfBirth}
+            defaultValue={formData.birthdate}
             onChange={event =>
-              setFormData({ ...formData, dateOfBirth: event.target.value })
+              setFormData({ ...formData, birthdate: event.target.value })
             }
             placeholder="Type date of birth"
           />
