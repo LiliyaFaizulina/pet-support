9d3fda4aaa9dc21ce244189226a590e8382d78da
LiliyaFaizulina <104977350+LiliyaFaizulina@users.noreply.github.com>
2023-01-19 16:28:28 +0000

    Deploying to gh-pages from @ LiliyaFaizulina/pet-support@6fff3e6c8339a79728a5b50aee89eea5079423c6 🚀

--- static/js/982.30ff9293.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpet_support=self.webpackChunkpet_support||[]).push([[982],{982:function(e,p,t){t.r(p);var u=t(184);p.default=function(){return(0,u.jsx)("div",{children:"User page"})}}}]);
-//# sourceMappingURL=982.30ff9293.chunk.js.map
