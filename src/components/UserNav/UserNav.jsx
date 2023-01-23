0d50588da4bf177fbaf9756931bed46e5aeefa15
ZVA-23 <zva-23@ukr.net>
2023-01-23 10:37:56 +0200

    fix slyles

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -1,13 +1,13 @@
-import { UserNavLink, UserNavIcon } from './UserNav.styled';
+import { UserNavBox, UserNavLink, UserNavIcon } from './UserNav.styled';
 import AccountIcon from '../../images/header/account.svg';
 
 export const UserNav = () => {
   return (
-    <>
+    <UserNavBox>
       <UserNavLink to="/user">
         <UserNavIcon src={AccountIcon} alt="account" />
         Account
       </UserNavLink>
-    </>
+    </UserNavBox>
   );
 };
